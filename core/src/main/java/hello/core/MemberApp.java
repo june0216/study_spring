006894e250b90fb5e5b4ec84b9ebebf1082cf073
june0216 <ziyun1612@ewhain.net>
2022-08-17 19:37:26 +0900

    3. 스프링 컨테이너 적용

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -2,12 +2,21 @@ package hello.core;
 
 import hello.core.member.*;
 import hello.core.order.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 	public static void main(String[] args) {
 		//MemberService memberService = new MemberServiceImpl();
-		AppConfig appConfig = new AppConfig();
-		MemberService memberService = appConfig.memberService(); // memberService 인터페이스를 불러오고 "memberService"에는 만들어진 구현체가 들어갈 것이다.
+		/*AppConfig appConfig = new AppConfig();
+		MemberService memberService = appConfig.memberService(); */// memberService 인터페이스를 불러오고 "memberService"에는 만들어진 구현체가 들어갈 것이다.
+
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);//스프링이다. 스프링은 이것으로 시작된다. 빈들을 관리해준다.
+		//어노테이션 기반으로 Config를 하고 있다. AppConfig에 있는 환경 설정들에 대한 정보를 기반으로 스프링 컨테이너에서 관리하게 해준다.
+
+		MemberService memberService = applicationContext.getBean("memberService", MemberService.class); //빈을 꺼낸다, 함수이름과 반환타입을 파라미터에 넣는다.
+
+
 		Member member = new Member(1L, "mamberA", Grade.VIP);
 		memberService.join(member);
 
--- core/src/main/java/hello/core/order/AppConfig.java
+++ core/src/main/java/hello/core/order/AppConfig.java
@@ -1,21 +1,36 @@
 package hello.core.order;
 
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //어플리케이션의 구성정보를 담당한다.
 public class AppConfig {
 
+	@Bean //스프링 컨테이너에 담긴다.
 	public MemberService memberService()
 	{
-		return new MemberServiceImpl(new MemoryMemberRepository()); //구현체를 밖에서 생성(생성자 안에 들어가서 주입됨 -> 생성자주입)
+		return new MemberServiceImpl(memberRepository()); //구현체를 밖에서 생성(생성자 안에 들어가서 주입됨 -> 생성자주입)
 	}//MemberServiceImpl을 생성하면서 이때 사용할 구현체는 MemoryMemberRepository()라고 객체를 생성하면서 알려주는 것이다.
+	@Bean
+	public MemberRepository memberRepository() { //따로 함수를 빼면 더 역할이 잘 보인다.
+		return new MemoryMemberRepository();
+	}
 
-
+	@Bean
 	public OrderService orderService()
 	{
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(memberRepository(), discountPolicy());
+	}
+	@Bean
+	public RateDiscountPolicy discountPolicy() {
+		//return new FixDiscountPolicy();
+		return new RateDiscountPolicy();
 	}
 
 
