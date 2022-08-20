1bfa07d7c6ab1d18c4c925826c97923968fce7a6
june0216 <ziyun1612@ewhain.net>
2022-08-20 19:25:36 +0900

    5. 싱글톤 컨테이너 주의사항

---
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,43 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+	@Test
+	void statefulServiceSingleton()
+	{
+		ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+		StatefulService statefulService1 = ac.getBean(StatefulService.class);
+		StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+		//ThreadA : A 사용자 10000원 주문
+		statefulService1.order("userA", 10000);
+		//ThreadB : B 사용자 10000원 주문
+		statefulService2.order("userB", 20000);
+
+		//ThreadA : 사용자A 주문 금액 조회
+		int price = statefulService1.getPrice();
+		System.out.println("price = " + price); //20000원이 나온다. -> statefulService 1 이든, 2이든 다 같은 객체를 참조하고 있는 싱글톤이기 때문
+
+		Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+
+
+	}
+
+	static class TestConfig
+	{
+
+		@Bean
+		public StatefulService statefulService() {
+			return new StatefulService();
+		}
+	}
+
+}
