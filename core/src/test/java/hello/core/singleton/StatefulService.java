390008afabfe072c08f2340a8f9a8cbe1127ef24
june0216 <ziyun1612@ewhain.net>
2022-08-21 15:49:28 +0900

    5. 싱글톤 컨테이너 주의사항 - 무상태를 유지하게 코드 변경

--- core/src/test/java/hello/core/singleton/StatefulService.java
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -1,6 +1,7 @@
 package hello.core.singleton;
 
 public class StatefulService {
+	/* 변경 전
 	private int price; // 상태를 유지하는 필드
 
 	public void order(String name, int price)
@@ -8,10 +9,14 @@ public class StatefulService {
 		System.out.println("name = " + name + "price = " + price);
 		this.price = price;
 
-	}
+	}*/
 
-	public int getPrice() {
+	//해결 = 전역 변수로 변경하기
+	public int order(String name, int price)
+	{
+		System.out.println("name = " + name + "price = " + price);
 		return price;
+
 	}
 
 }
--- core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -18,15 +18,15 @@ class StatefulServiceTest {
 		StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
 		//ThreadA : A 사용자 10000원 주문
-		statefulService1.order("userA", 10000);
+		int userAPrice = statefulService1.order("userA", 10000);
 		//ThreadB : B 사용자 10000원 주문
-		statefulService2.order("userB", 20000);
+		int userBPrice = statefulService2.order("userB", 20000);
 
 		//ThreadA : 사용자A 주문 금액 조회
-		int price = statefulService1.getPrice();
-		System.out.println("price = " + price); //20000원이 나온다. -> statefulService 1 이든, 2이든 다 같은 객체를 참조하고 있는 싱글톤이기 때문
+		//int price = statefulService1.getPrice();
+		System.out.println("price = " + userAPrice); //20000원이 나온다. -> statefulService 1 이든, 2이든 다 같은 객체를 참조하고 있는 싱글톤이기 때문
 
-		Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+		//Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
 
 
 	}
